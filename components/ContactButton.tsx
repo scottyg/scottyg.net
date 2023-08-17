75d04f2281d57f6bded7e7ab15f860c41477a37c
Scott Gordon <me@scottyg.net>
2023-08-17 10:56:10 -0700

    Update ContactButton.tsx

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -32,9 +32,11 @@ export function ContactButton(props: ButtonProps) {
 
     color = tw`px-2 py-1 inline-block rounded-md bg-blue-400 hover:bg-blue-700 text-white mr-2 group cursor-pointer`;
     logo = (
-      <svg fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1"
+      <svg fill="#000000" height="256px" width="256px" version="1.1" id="Layer_1"
       	xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-      	viewBox="0 0 310 310" xml:space="preserve">
+      	viewBox="0 0 310 310" xml:space="preserve"
+        class={tw`w-4 h-4 inline-block -mt-1 mr-1 text-white fill-current stroke-transparent`}
+        >
       	<g id="XMLID_801_">
       		<path id="XMLID_802_" d="M72.16,99.73H9.927c-2.762,0-5,2.239-5,5v199.928c0,2.762,2.238,5,5,5H72.16c2.762,0,5-2.238,5-5V104.73
       			C77.16,101.969,74.922,99.73,72.16,99.73z"/>
